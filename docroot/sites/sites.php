111373e8dd70a6f4b6e13b98660122cb8cb772df
Brad Fisher <baf139@psu.edu>
2020-07-12 18:34:44 +0000

    ready

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4,3 +4,4 @@ $sites['seostatecollege.fws.test'] = 'default';
 $sites['seostatecollege.com'] = 'default';
 $sites['ikkd8.fws.test'] = 'insidekellyskitchen';
 $sites['dev.insidekellyskitchen.com'] = 'insidekellyskitchen';
+$sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
