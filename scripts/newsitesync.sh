688c67c119acda8ef390ee67c8ce8f21df7c6a06
Brad Fisher <baf139@psu.edu>
2020-08-07 22:28:44 +0000

    fuck

--- scripts/newsitesync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-site=$1
-# To sync a single site to your local just type the domain after the script name as arg1 and your user name as arg2
-sudo chmod -R 777 /var/www/html/fws/docroot/sites/lg/files
-drush sql-sync @local.lg @prod.$site -y
-drush -y rsync @local.lg:%files @prod.$site:%files --mode=zOvr --exclude-paths=css:js:php:private:styles:tmp:xmlsitemap
-sudo chmod -R 755 /var/www/html/fws/docroot/sites/lg/files
--- scripts/prodnewsite.sh
+++ scripts/prodnewsite.sh
@@ -15,7 +15,7 @@ sed -i "s/rootpass/BafLjfMySql_2019/g" /var/www/html/fws/docroot/sites/$site/set
 
 mkdir -p $site/files/
 cp lg/files/* $site/files/ -rf
-chown -R www-data:fws $site/files/
+chown -R www-data:www-data $site/files/
 chown fws:fws $site
 chown fws:fws $site/settings.php
 chmod 644 $site/settings.php
@@ -24,7 +24,8 @@ chmod -R 755 $site/files
 # Create the DB first since we have global read, a .my.cnf file and no grant option.
 ##########################################################
 mysql -e "create database if not exists $prefix$site;"
-
+mysqldump fws_lg > /var/www/$site.sql
+mysql fws_$site < /var/www/$site.sql
 
 cat /dev/null > /var/www/html/fws/docroot/sites/sites.php
 echo "<?php" >> /var/www/html/fws/docroot/sites/sites.php
@@ -48,5 +49,3 @@ echo "##### Adding server config /etc/hosts on term and enable the conf."
 cat /var/www/html/fws/scripts/prod.vhost.txt > /etc/apache2/sites-available/$site.conf
 sed -i "s/website/$site/g" /etc/apache2/sites-available/$site.conf
 a2ensite $site
-echo "run newsitesync.sh on local as vagrant"
-
