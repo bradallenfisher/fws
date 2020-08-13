212ef57450179f769ee0a43104858451ba970206
Brad Fisher <baf139@psu.edu>
2020-08-13 21:37:26 +0000

    gallery furk

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,36 +2,36 @@
 
 
 $sites['www.homeremodelinglincoln.fisherwebsolutions.com'] = 'homeremodelinglincoln';
-$sites['homeremodelinglincoln.fws.test'] = 'homeremodelinglincoln';
+$sites['www.homeremodelinglincoln.fws.test'] = 'homeremodelinglincoln';
 $sites['homeremodelinglincoln.com'] = 'homeremodelinglincoln';
 $sites['www.homeremodelinglincoln.com'] = 'homeremodelinglincoln';
 
 
 $sites['www.insidekellyskitchen.fisherwebsolutions.com'] = 'insidekellyskitchen';
-$sites['insidekellyskitchen.fws.test'] = 'insidekellyskitchen';
+$sites['www.insidekellyskitchen.fws.test'] = 'insidekellyskitchen';
 $sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
 $sites['www.insidekellyskitchen.com'] = 'insidekellyskitchen';
 
 
 $sites['www.lg.fisherwebsolutions.com'] = 'lg';
-$sites['lg.fws.test'] = 'lg';
+$sites['www.lg.fws.test'] = 'lg';
 $sites['lg.com'] = 'lg';
 $sites['www.lg.com'] = 'lg';
 
 
 $sites['www.rvfastfix.fisherwebsolutions.com'] = 'rvfastfix';
-$sites['rvfastfix.fws.test'] = 'rvfastfix';
+$sites['www.rvfastfix.fws.test'] = 'rvfastfix';
 $sites['rvfastfix.com'] = 'rvfastfix';
 $sites['www.rvfastfix.com'] = 'rvfastfix';
 
 
 $sites['www.seostatecollege.fisherwebsolutions.com'] = 'seostatecollege';
-$sites['seostatecollege.fws.test'] = 'seostatecollege';
+$sites['www.seostatecollege.fws.test'] = 'seostatecollege';
 $sites['seostatecollege.com'] = 'seostatecollege';
 $sites['www.seostatecollege.com'] = 'seostatecollege';
 
 
 $sites['www.tuscaloosalawncare.fisherwebsolutions.com'] = 'tuscaloosalawncare';
-$sites['tuscaloosalawncare.fws.test'] = 'tuscaloosalawncare';
+$sites['www.tuscaloosalawncare.fws.test'] = 'tuscaloosalawncare';
 $sites['tuscaloosalawncare.com'] = 'tuscaloosalawncare';
 $sites['www.tuscaloosalawncare.com'] = 'tuscaloosalawncare';
