49578e5824980c7a17b96ef0ef56954eb7be4d2f
Brad Fisher <baf139@psu.edu>
2020-08-14 00:22:31 +0000

    code

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,37 +1,50 @@
 <?php
 
 
+$sites['www.greensborofoundation.fisherwebsolutions.com'] = 'greensborofoundation';
+$sites['greensborofoundation.fisherwebsolutions.com'] = 'greensborofoundation';
+$sites['greensborofoundation.fws.test'] = 'greensborofoundation';
+$sites['greensborofoundation.com'] = 'greensborofoundation';
+$sites['www.greensborofoundation.com'] = 'greensborofoundation';
+
+
 $sites['www.homeremodelinglincoln.fisherwebsolutions.com'] = 'homeremodelinglincoln';
-$sites['www.homeremodelinglincoln.fws.test'] = 'homeremodelinglincoln';
+$sites['homeremodelinglincoln.fisherwebsolutions.com'] = 'homeremodelinglincoln';
+$sites['homeremodelinglincoln.fws.test'] = 'homeremodelinglincoln';
 $sites['homeremodelinglincoln.com'] = 'homeremodelinglincoln';
 $sites['www.homeremodelinglincoln.com'] = 'homeremodelinglincoln';
 
 
 $sites['www.insidekellyskitchen.fisherwebsolutions.com'] = 'insidekellyskitchen';
-$sites['www.insidekellyskitchen.fws.test'] = 'insidekellyskitchen';
+$sites['insidekellyskitchen.fisherwebsolutions.com'] = 'insidekellyskitchen';
+$sites['insidekellyskitchen.fws.test'] = 'insidekellyskitchen';
 $sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
 $sites['www.insidekellyskitchen.com'] = 'insidekellyskitchen';
 
 
 $sites['www.lg.fisherwebsolutions.com'] = 'lg';
-$sites['www.lg.fws.test'] = 'lg';
+$sites['lg.fisherwebsolutions.com'] = 'lg';
+$sites['lg.fws.test'] = 'lg';
 $sites['lg.com'] = 'lg';
 $sites['www.lg.com'] = 'lg';
 
 
 $sites['www.rvfastfix.fisherwebsolutions.com'] = 'rvfastfix';
-$sites['www.rvfastfix.fws.test'] = 'rvfastfix';
+$sites['rvfastfix.fisherwebsolutions.com'] = 'rvfastfix';
+$sites['rvfastfix.fws.test'] = 'rvfastfix';
 $sites['rvfastfix.com'] = 'rvfastfix';
 $sites['www.rvfastfix.com'] = 'rvfastfix';
 
 
 $sites['www.seostatecollege.fisherwebsolutions.com'] = 'seostatecollege';
-$sites['www.seostatecollege.fws.test'] = 'seostatecollege';
+$sites['seostatecollege.fisherwebsolutions.com'] = 'seostatecollege';
+$sites['seostatecollege.fws.test'] = 'seostatecollege';
 $sites['seostatecollege.com'] = 'seostatecollege';
 $sites['www.seostatecollege.com'] = 'seostatecollege';
 
 
 $sites['www.tuscaloosalawncare.fisherwebsolutions.com'] = 'tuscaloosalawncare';
-$sites['www.tuscaloosalawncare.fws.test'] = 'tuscaloosalawncare';
+$sites['tuscaloosalawncare.fisherwebsolutions.com'] = 'tuscaloosalawncare';
+$sites['tuscaloosalawncare.fws.test'] = 'tuscaloosalawncare';
 $sites['tuscaloosalawncare.com'] = 'tuscaloosalawncare';
 $sites['www.tuscaloosalawncare.com'] = 'tuscaloosalawncare';
