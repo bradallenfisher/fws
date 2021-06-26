7eb3ce377cc5c03f530a96759d9606fe5bd804a0
bradallenfisher <bradallenfisher@gmail.com>
2021-06-26 17:17:05 -0400

    composer updates

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -708,6 +708,7 @@ class ComposerStaticInitec6b7acc9d286cc19b3b0df221b189aa
         'ArithmeticError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
         'AssertionError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/AssertionError.php',
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'DivisionByZeroError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/DivisionByZeroError.php',
         'Drupal' => __DIR__ . '/../..' . '/docroot/core/lib/Drupal.php',
         'DrupalFinder\\DrupalFinder' => __DIR__ . '/..' . '/webflo/drupal-finder/src/DrupalFinder.php',
