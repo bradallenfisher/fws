3f356e035af1de4a2f8055381877529bdb4a48f4
bradallenfisher <bradallenfisher@gmail.com>
2021-06-27 08:03:15 -0400

    sites

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,11 +1,15 @@
 <?php
-$sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
+
+
 $sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
 $sites['www.insidekellyskitchen.com'] = 'insidekellyskitchen';
 $sites['www.insidekellyskitchen.test'] = 'insidekellyskitchen';
+
 $sites['rvfastfix.com'] = 'rvfastfix';
 $sites['www.rvfastfix.com'] = 'rvfastfix';
+
 $sites['seostatecollege.com'] = 'seostatecollege';
 $sites['www.seostatecollege.com'] = 'seostatecollege';
+
 $sites['www.lgd8.bludigitalsolutions.com'] = 'lgd8';
 $sites['www.dt.fisherwebsolutions.com'] = 'dt';
