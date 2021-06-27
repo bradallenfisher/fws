26f49b9228519612e2147c2a5d0cb492085a8bb4
brad <bradallenfisher@gmail.com>
2021-06-27 21:50:21 +0000

    ads

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -3,13 +3,9 @@
 
 $sites['insidekellyskitchen.com'] = 'insidekellyskitchen';
 $sites['www.insidekellyskitchen.com'] = 'insidekellyskitchen';
-$sites['www.insidekellyskitchen.test'] = 'insidekellyskitchen';
+$sites['insidekellyskitchen.test'] = 'insidekellyskitchen';
 
 $sites['rvfastfix.com'] = 'rvfastfix';
 $sites['www.rvfastfix.com'] = 'rvfastfix';
 
-$sites['seostatecollege.com'] = 'seostatecollege';
-$sites['www.seostatecollege.com'] = 'seostatecollege';
 
-$sites['www.lgd8.bludigitalsolutions.com'] = 'lgd8';
-$sites['www.dt.fisherwebsolutions.com'] = 'dt';
